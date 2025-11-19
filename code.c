49c0b08268bf8bd3f3eab06f1511427b1f21ffff
ojasvinodkute <ojaskute2007@gmail.com>
2025-11-19 16:36:41 +0530

    Refactor savings calculation and add recursive function

--- code.c
+++ code.c
@@ -1,26 +1,19 @@
-#include <stdio.h>
-
-int calculateSavings(int *day, int totalDays)
-{
-    if (*day > totalDays) return 0;
-    int today = *day;
-    (*day)++;
-    return today + calculateSavings(day, totalDays);
+ #include <stdio.h>
+int fun(int n) {
+    if (n == 1)
+    return 1;
+    else
+    return n+fun(n - 1);
 }
-
-int main()
-{
-    int totalDays;
-    int day = 1;
-    if (printf("Enter how many days you saved money: ") && scanf("%d", &totalDays))
-    {
-        if (totalDays <= 0)
-        {
-            printf("Total savings in %d days = 0\n", totalDays);
-            return 0;
-        }
-        int total = calculateSavings(&day, totalDays);
-        printf("Total savings in %d days = %d\n", totalDays, total);
+int main(){
+    int n;
+    printf("Enter how many days you saved money  ");
+    scanf("%d", &n);
+    if (n<=0)
+    printf("Please enter a positive integer\n");
+    else {
+        int sum =fun(n);
+        printf("Total savings in %d days is : %d\n" n, sum);
     }
     return 0;
 }
